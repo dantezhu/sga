738ad908c983fd1dd75db73fe06fb2cd97faa4b6
dantezhu <dantezhu@qq.com>
2014-02-26 20:16:23 +0800

    测试通过

--- sga/demo/kola_ga_demo.py
+++ sga/demo/kola_ga_demo.py
@@ -6,7 +6,10 @@ from sga.kola_ga import KolaGA
 app = Kola()
 ga = KolaGA(dict(
     GA_ID='UA-46303840-3',
-    GA_ALLOW_PATHS=[r'^index']
+    GA_ALLOW_PATHS=[r'^index'],
+    GA_HACK_PATHS=[
+        (r'in(\S+)', r'\g<1>/ok'),
+        ]
 ), app)
 
 
